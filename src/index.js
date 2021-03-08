fe3c52f515f7829fd1bbc5632ca55d8442eab9ad
flipxonsoft <parthmalakar4@gmail.com>
2021-03-08 14:27:54 +0530

    created store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './componenets/App';
+import movies from './reducers/index'
+
+
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('State', store.getState());
 
 
 ReactDOM.render(
