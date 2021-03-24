315d87bc86750ceeaf469be8892e7a22f91747d4
flipxonsoft <parthmalakar4@gmail.com>
2021-03-24 13:59:40 +0530

    middle ware and modifying middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './componenets/App';
 import rootReducer from './reducers/index';
 
+// function logger(obj, next, action)
+// logger(obj)(next)(action)
+// const logger = function ({dispatch, getState}) {
+//   return function (next) {
+//     return function (action) {
+      // middleware code
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  // middleware code
+  console.log('ACTION_TYPE = ', action.type);
+  next(action);
+}
 
 
-const store = createStore(rootReducer);
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store', store);
 // console.log('Before State', store.getState());
 
